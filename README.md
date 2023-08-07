c7e333457d66f2862df300c140e80cdf1ff1adca
sunkang <13127955213@qq.com>
2023-08-07 18:40:33 +0800

    基础函数

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
 # 基于URL的权限管理
 
+# 添加权限
 
+~~~
+url_acl_add('产品列表','/index.php/goods');
+url_acl_add('产品','/index.php/goods/add');
+~~~
+
+# 取权限信息
+
+~~~
+print_r(get_url_acl());
+~~~
+
+# 对角色赋予权限
+
+~~~
+url_acl_add_auth("管理员",'产品');
+url_acl_add_auth("管理员",'产品列表');
+~~~
+
+# 查看角色可以访问的URL
+
+~~~
+$res = get_url_acl_auth_urls("管理员");
+print_r($res);
+~~~
+
+# 检测是否有权限
+
+~~~
+print_r(url_acl("管理员"));
+~~~
 
 # 安装
 
@@ -8,7 +39,9 @@
 ~~~
 "thefunpower/url_acl": "dev-main" 
 ~~~
- 
+
+
+
 
 ### 开源协议 
 
--- helper.php
+++ helper.php
@@ -1,3 +1,90 @@
 <?php 
+ 
+/**
+* 向角色中添加权限名
+* url_acl_add_auth("管理员",'产品');
+* url_acl_add_auth("管理员",['产品','产品列表']);
+*/
+function url_acl_add_auth($auth_name,$acl_title){
+	global $_url_acl_add_auth;
+	if(is_array($acl_title)){
+		foreach($acl_title as $title){
+			$_url_acl_add_auth[$auth_name][$title] = $title;
+		}
+	}else{
+		$_url_acl_add_auth[$auth_name][$acl_title] = $acl_title;	
+	}	
+}
+/**
+* 检测角色是否有权限
+*/
+function url_acl($auth_name){
+	$list = get_url_acl_auth_urls($auth_name);
+	if(!$list){
+		return false;
+	}
+	$uri = get_url_acl_request_uri();
+	if($uri && in_array($uri,$list)){
+		return true;
+	}else{
+		return false;
+	}
+}
+/**
+* 取角色有的权限
+* 返回可以访问的URL
+*/
+function get_url_acl_auth_urls($auth_name){ 
+	global $_url_acl_list; 
+	$list = get_url_acl_auth_name($auth_name);
+	if(!$list){
+		return ;
+	}else{
+		$urls = [];
+		foreach($list as $k=>$v){ 
+			$urls[] = $_url_acl_list[$v]; 
+		}
+		return $urls;
+	}
+}
+/**
+* 取角色拥有的ACL名称
+*/
+function get_url_acl_auth_name($auth_name){
+	global $_url_acl_add_auth; 
+	if(!is_array($_url_acl_add_auth) || !isset($_url_acl_add_auth[$auth_name])){
+		return;
+	}
+	$list = $_url_acl_add_auth[$auth_name];
+	return $list;
+}
 
 
+/**
+* 把URL添加到权限中
+*/
+function url_acl_add($title,$url){
+	global $_url_acl_list;
+	$_url_acl_list[$title] = $url;
+}
+/**
+* 取请求URL，不包含?
+*/
+function get_url_acl_request_uri(){
+	$uri = $_SERVER['REQUEST_URI'];
+	if(!$uri){
+		echo 'REQUEST_URI not in _SERVER';exit;
+	}
+	if(strpos($uri,'?')!==false){
+		$uri = substr($uri,0,strpos($uri,'?'));
+	}
+	return $uri;
+}
+
+/**
+* 权限列表
+*/
+function get_url_acl(){
+	global $_url_acl_list;
+	return $_url_acl_list;
+}
